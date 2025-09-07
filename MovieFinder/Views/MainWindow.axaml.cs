bfbb1cca6fcefbda73970a34f49268ec0279ae6e
Jesse Stone <jesse@elderlinksolutions.com>
2025-09-07 15:33:25 -0700

    Removed debug loop

--- MovieFinder/Views/MainWindow.axaml.cs
+++ MovieFinder/Views/MainWindow.axaml.cs
@@ -31,21 +31,6 @@ public partial class MainWindow : Window
         }
     }
 
-    private void OpenDebugWindow(object? sender, PointerPressedEventArgs e)
-    {
-        if (DataContext is MainWindowViewModel vm && vm.BarcodeService is not null)
-        {
-            var logger = App.Services?.GetRequiredService<IAppLogger>();
-            var debugWindowViewModel = new DebugWindowViewModel(vm.BarcodeService, logger);
-            var debugWindow = new DebugWindow
-            {
-                DataContext = debugWindowViewModel
-            };
-            logger?.Log("Debug Window Initialized.");
-            debugWindow.Show();
-        }
-    }
-
     public void BeginMoveDrag(object? sender, PointerPressedEventArgs e)
     {
         BeginMoveDrag(e);
